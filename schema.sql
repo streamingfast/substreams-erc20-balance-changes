09ee88f43c77d8d3ec927ceca043904b6853da4c
colindickson <colin.dickson@gmail.com>
2024-11-07 09:40:18 -0500

    fix sql schema and primary key

--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS balance_changes (
-   `contract` TEXT NOT NULL,
-   `owner` TEXT NOT NULL,
-   `amount` INT NOT NULL,
-   `old_balance` INT NOT NULL,
-   `new_balance` INT NOT NULL,
-   `transaction_id` TEXT NOT NULL,
-   `block_num` INT NOT NULL,
-   `timestamp` TEXT NOT NULL,
-   `change_type` INT NOT NULL,
+   "contract" TEXT NOT NULL,
+   "owner" TEXT NOT NULL,
+   "amount" NUMERIC NOT NULL,
+   "old_balance" NUMERIC NOT NULL,
+   "new_balance" NUMERIC NOT NULL,
+   "transaction_id" TEXT NOT NULL,
+   "block_num" INT NOT NULL,
+   "timestamp" TEXT NOT NULL,
+   "change_type" INT NOT NULL,
+   "call_index" INT NOT NULL,
+   PRIMARY KEY ("block_num", "transaction_id", "call_index")
 );
--- src/sinks.rs
+++ src/sinks.rs
@@ -33,23 +33,26 @@ pub fn graph_out(_clock: Clock, balance_changes: BalanceChanges) -> Result<Entit
 #[substreams::handlers::map]
 pub fn db_out(clock: Clock, balance_changes: BalanceChanges) -> Result<DatabaseChanges, Error> {
     let mut tables = substreams_database_change::tables::Tables::new();
-    let block_num = clock.number.to_string();
-    let timestamp = clock.timestamp.unwrap().seconds.to_string();
+    let block_num = clock.clone().number.to_string();
+    let timestamp = clock.clone().timestamp.unwrap().seconds.to_string();
 
     for balance_change in balance_changes.balance_changes {
         if balance_change.change_type == 0 {
             continue;
         }
 
-        let key = format!("{}:{}:{}", balance_change.contract, balance_change.owner, balance_change.transaction);
-
-        tables.create_row("balance_changes", key)
+        tables.create_row("balance_changes", [
+            ("block_num", (&clock).number.to_string()),
+            ("transaction_id", (&balance_change).transaction.to_string()),
+            ("call_index", (&balance_change).call_index.to_string())
+        ])
             .set("contract", balance_change.contract)
             .set("owner", balance_change.owner)
             .set("amount", balance_change.transfer_value)
             .set("old_balance", balance_change.old_balance)
             .set("new_balance", balance_change.new_balance)
             .set("transaction_id", balance_change.transaction)
+            .set("call_index", balance_change.call_index)
             .set("block_num", &block_num)
             .set("timestamp", &timestamp)
             .set("change_type", balance_change.change_type);
