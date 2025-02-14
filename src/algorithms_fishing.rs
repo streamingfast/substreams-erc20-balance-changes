b191973775c0fd3be78083fba9cb76ceeac92db5
Denis Carriere <carriere.denis@gmail.com>
2025-02-14 14:48:06 -0500

    Fix fishing algo https://github.com/streamingfast/substreams-erc20-balance-changes/issues/18

--- src/algorithms_fishing.rs
+++ src/algorithms_fishing.rs
@@ -6,10 +6,14 @@ use crate::algorithms::get_all_child_calls;
 // Ignore fishing token transfers, typically do not have any storage changes associated with them or child calls
 // https://github.com/streamingfast/substreams-erc20-balance-changes/issues/18
 pub fn ignore_fishing_transfers(trx: &TransactionTrace, call: &Call) -> bool {
+    let mut count = call.storage_changes.len();
     let child_calls = get_all_child_calls(&call, &trx);
-    if call.storage_changes.is_empty() && child_calls.is_empty() {
-        info!("ignoring fishing token transfer trx: {:?}", trx.hash);
-        return false;
+    for child_call in child_calls {
+        count += child_call.storage_changes.len();
     }
-    return true;
+    if count == 0 {
+        return true;
+    }
+    info!("ignoring fishing token transfer trx: {:?}", trx.hash);
+    return false;
 }
