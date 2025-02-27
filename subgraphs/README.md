8eda220bc46e2577211962ebfd57fa1e6395f2da
Denis Carriere <carriere.denis@gmail.com>
2025-02-27 18:24:16 -0500

    update Subgraphs

--- subgraphs/README.md
+++ subgraphs/README.md
@@ -3,3 +3,6 @@
 ## Quickstart
 
 > ... TO-DO ...
+
+Timestamp support has been added in v0.36.0
+https://github.com/graphprotocol/graph-node/releases/tag/v0.36.0
--- subgraphs/src/graph_out.rs
+++ subgraphs/src/graph_out.rs
@@ -8,34 +8,13 @@ pub fn graph_out(events: Events) -> Result<EntityChanges, Error> {
 
     for balance_change in events.balance_changes {
         tables
-            .create_row(
-                "BalanceChange",
-                format!("{}:{}", &balance_change.block_num, &balance_change.storage_ordinal),
-            )
-            // -- block --
-            .set_bigint("block_num", &balance_change.block_num.to_string())
-            .set("block_hash", &balance_change.block_hash)
-            // Timestamp support has been added in v0.36.0
-            // https://github.com/graphprotocol/graph-node/releases/tag/v0.36.0
-            .set("timestamp", balance_change.timestamp.expect("missing timestamp"))
-            .set("date", &balance_change.date)
-
-            // -- transaction --
-            .set("transaction_id", &balance_change.transaction_id)
-
-            // -- balance change --
-            .set("contract", &balance_change.contract)
-            .set("owner", &balance_change.owner)
-            .set_bigint("old_balance", &balance_change.old_balance)
-            .set_bigint("new_balance", &balance_change.new_balance);
-
-        tables
             .create_row("Balance", format!("{}:{}", balance_change.contract, balance_change.owner))
             // -- block --
             .set_bigint("block_num", &balance_change.block_num.to_string())
-            .set("block_hash", &balance_change.block_hash)
             .set("timestamp", balance_change.timestamp.expect("missing timestamp"))
             .set("date", balance_change.date)
+            // -- transaction --
+            .set("transaction_id", balance_change.transaction_id)
             // -- current balance --
             .set("contract", balance_change.contract)
             .set("owner", balance_change.owner)
@@ -47,7 +26,6 @@ pub fn graph_out(events: Events) -> Result<EntityChanges, Error> {
             .create_row("Transfer", format!("{}:{}", transfer.block_num, transfer.log_block_index))
             // -- block --
             .set_bigint("block_num", &transfer.block_num.to_string())
-            .set("block_hash", transfer.block_hash)
             .set("timestamp", transfer.timestamp.expect("missing timestamp"))
             .set("date", transfer.date)
             // -- transaction --
