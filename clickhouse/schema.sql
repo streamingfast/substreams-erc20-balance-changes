80947c39caa28aaf67df87213da359dad14bdfb7
Denis Carriere <carriere.denis@gmail.com>
2025-03-13 17:20:08 -0400

    remove indent

--- clickhouse/schema.sql
+++ clickhouse/schema.sql
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS transfers  (
    INDEX idx_transfers_contract (contract)  TYPE bloom_filter GRANULARITY 4,
    INDEX idx_transfers_from     (`from`)    TYPE bloom_filter GRANULARITY 4,
    INDEX idx_transfers_to       (`to`)      TYPE bloom_filter GRANULARITY 4,
-   INDEX idx_transfers_from_or_to (concat(`from`, '#', `to`)) TYPE bloom_filter GRANULARITY 4;
+   INDEX idx_transfers_from_or_to (concat(`from`, '#', `to`)) TYPE bloom_filter GRANULARITY 4
 )
 ENGINE = ReplacingMergeTree
 PRIMARY KEY (block_num, ordinal)
