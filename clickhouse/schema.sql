a3fbc69860f29d47584211da2fa4df78f2d426b1
Denis Carriere <carriere.denis@gmail.com>
2025-04-08 11:36:32 -0400

    update ohlc

--- clickhouse/schema.sql
+++ clickhouse/schema.sql
@@ -694,23 +694,29 @@ CREATE TABLE IF NOT EXISTS ohlc_prices (
    timestamp            DateTime(0, 'UTC') COMMENT 'the start of the aggregate window',
 
    -- pool --
-   factory              FixedString(42) COMMENT 'factory address',
    pool                 FixedString(42) COMMENT 'pool address',
-   token0               FixedString(42) COMMENT 'token0 address',
-   token1               FixedString(42) COMMENT 'token1 address',
 
    -- swaps --
-   open                 AggregateFunction(argMin, Float64, UInt64),
-   high                 SimpleAggregateFunction(max, Float64),
-   low                  SimpleAggregateFunction(min, Float64),
-   close                AggregateFunction(argMax, Float64, UInt64),
+   open0                AggregateFunction(argMin, Float64, UInt64),
+   high0                SimpleAggregateFunction(max, Float64),
+   low0                 SimpleAggregateFunction(min, Float64),
+   close0               AggregateFunction(argMax, Float64, UInt64),
+   volume0              AggregateFunction(sum, Float64),
+
+   -- swaps (inverse) --
+   open1                AggregateFunction(argMin, Float64, UInt64),
+   high1                SimpleAggregateFunction(max, Float64),
+   low1                 SimpleAggregateFunction(min, Float64),
+   close1               AggregateFunction(argMax, Float64, UInt64),
+   volume1              AggregateFunction(sum, Float64),
+
+   -- universal --
    uaw                  AggregateFunction(uniq, FixedString(42)) COMMENT 'unique wallet addresses in the window',
-   transactions         AggregateFunction(sum, UInt8) COMMENT 'number of transactions in the window',
-   volume               AggregateFunction(sum, Float64) COMMENT 'total volume in token0 currency',
+   transactions         AggregateFunction(sum, UInt8) COMMENT 'number of transactions in the window'
 )
 ENGINE = AggregatingMergeTree
-PRIMARY KEY (factory, pool, token0, token1, timestamp)
-ORDER BY (factory, pool, token0, token1, timestamp);
+PRIMARY KEY (pool, timestamp)
+ORDER BY (pool, timestamp);
 
 -- Swaps --
 CREATE MATERIALIZED VIEW IF NOT EXISTS ohlc_swaps_mv
@@ -718,46 +724,26 @@ TO ohlc_prices
 AS
 SELECT
    toStartOfHour(timestamp) AS timestamp,
-   factory,
-   s.pool AS pool,
-   p.token0 as token0,
-   p.token1 as token1,
-   argMinState(price, s.global_sequence) AS open,
-   quantileDeterministic(0.95)(price, s.global_sequence) AS high,
-   quantileDeterministic(0.05)(price, s.global_sequence) AS low,
-   argMaxState(price, s.global_sequence) AS close,
-   uniqState(sender) AS uaw,
-   sumState(1) AS transactions,
-   sumState(toDecimal256(amount0, 18) / pow(10, c.decimals) ) AS volume
-FROM swaps AS s
-JOIN pools AS p
-   ON s.pool = p.pool
-JOIN contracts AS c
-   ON p.token0 = c.address
-GROUP BY factory, pool, token0, token1, timestamp;
+   pool,
 
--- Swaps (Inverse) --
-CREATE MATERIALIZED VIEW IF NOT EXISTS ohlc_swaps_inverse_mv
-TO ohlc_prices
-AS
-SELECT
-   toStartOfHour(timestamp) AS timestamp,
-   factory,
-   s.pool AS pool,
-   p.token1 as token0,
-   p.token0 as token1,
-   argMinState(1 / price, s.global_sequence) AS open,
-   quantileDeterministic(0.95)(1 / price, s.global_sequence) AS high,
-   quantileDeterministic(0.05)(1 / price, s.global_sequence) AS low,
-   argMaxState(1 / price, s.global_sequence) AS close,
+   -- swaps --
+   argMinState(price, global_sequence) AS open0,
+   quantileDeterministic(0.95)(price, global_sequence) AS high0,
+   quantileDeterministic(0.05)(price, global_sequence) AS low0,
+   argMaxState(price, global_sequence) AS close0,
+   sumState(toDecimal256(amount0, 18) * price ) AS volume0,
+
+   -- swaps (inverse) --
+   argMinState(1 / price, global_sequence) AS open1,
+   quantileDeterministic(0.95)(1 / price, global_sequence) AS high1,
+   quantileDeterministic(0.05)(1 / price, global_sequence) AS low1,
+   argMaxState(1 / price, global_sequence) AS close1,
+   sumState(toDecimal256(amount0, 18) * (1 / price) ) AS volume1,
+
+   -- universal --
    uniqState(sender) AS uaw,
-   sumState(1) AS transactions,
-   sumState(toDecimal256(amount1, 18) / pow(10, c.decimals) ) AS volume
-FROM swaps AS s
-JOIN pools AS p
-   ON s.pool = p.pool
-JOIN contracts AS c
-   ON p.token1 = c.address
-GROUP BY factory, pool, token0, token1, timestamp;
+   sumState(1) AS transactions
+FROM swaps
+GROUP BY pool, timestamp;
 
 
