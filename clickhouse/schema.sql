6b47e69147e67cd4e319ae98c00f16149797fcd6
Denis Carriere <carriere.denis@gmail.com>
2025-04-10 14:36:14 -0400

    Add gross volume & net flow

--- clickhouse/schema.sql
+++ clickhouse/schema.sql
@@ -692,22 +692,30 @@ SELECT * FROM historical_balances;
 CREATE TABLE IF NOT EXISTS ohlc_prices (
    -- block --
    block_num            SimpleAggregateFunction(min, UInt32),
-   timestamp            DateTime(0, 'UTC') COMMENT 'the start of the aggregate window',
+   timestamp            DateTime(0, 'UTC') COMMENT 'beginning of the bar',
 
    -- pool --
-   pool                 FixedString(42) COMMENT 'pool address',
+   pool                 LowCardinality(FixedString(42)) COMMENT 'pool address',
 
    -- swaps --
    open0                AggregateFunction(argMin, Float64, UInt64),
    high0                AggregateFunction(quantileDeterministic, Float64, UInt64),
    low0                 AggregateFunction(quantileDeterministic, Float64, UInt64),
    close0               AggregateFunction(argMax, Float64, UInt64),
-   volume0              AggregateFunction(sum, Float64),
-   volume1              AggregateFunction(sum, Float64),
+
+   -- volume --
+
+   -- “Gross” or “volume” signals a total quantity traded with no regard to direction. --
+   gross_volume0        SimpleAggregateFunction(sum, UInt256) COMMENT 'gross volume of token0 in the window',
+   gross_volume1        SimpleAggregateFunction(sum, UInt256) COMMENT 'gross volume of token1 in the window',
+
+   -- “Net” plus “flow” tells you it’s a directional figure that can be positive or negative. --
+   net_flow0            SimpleAggregateFunction(sum, Int256) COMMENT 'net flow of token0 in the window',
+   net_flow1            SimpleAggregateFunction(sum, Int256) COMMENT 'net flow of token1 in the window',
 
    -- universal --
    uaw                  AggregateFunction(uniq, FixedString(42)) COMMENT 'unique wallet addresses in the window',
-   transactions         AggregateFunction(sum, UInt8) COMMENT 'number of transactions in the window'
+   transactions         SimpleAggregateFunction(sum, UInt64) COMMENT 'number of transactions in the window'
 )
 ENGINE = AggregatingMergeTree
 PRIMARY KEY (pool, timestamp)
@@ -730,12 +738,16 @@ SELECT
    quantileDeterministicState(0.99)(price, global_sequence) AS high0,
    quantileDeterministicState(0.01)(price, global_sequence) AS low0,
    argMaxState(price, global_sequence) AS close0,
-   sumState(toDecimal256(abs(amount0), 18) / pow(10, 18) ) AS volume0,
-   sumState(toDecimal256(abs(amount1), 18) / pow(10, 18) ) AS volume1,
+
+   -- volume --
+   sum(toUInt256(abs(amount0))) AS gross_volume0,
+   sum(toUInt256(abs(amount1))) AS gross_volume1,
+   sum(toInt256(amount0))     AS net_flow0,
+   sum(toInt256(amount1))     AS net_flow1,
 
    -- universal --
    uniqState(sender) AS uaw,
-   sumState(1) AS transactions
+   sum(1) AS transactions
 FROM swaps
 GROUP BY pool, timestamp;
 
