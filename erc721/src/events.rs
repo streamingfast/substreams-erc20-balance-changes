f94a64cd271aea5f5f91003c31e860a29f7fda63
YaroShkvorets <shkvorets@gmail.com>
2025-04-26 23:52:12 -0400

    remove duplicate code from erc721

--- erc721/src/events.rs
+++ erc721/src/events.rs
@@ -1,104 +1,29 @@
 use crate::pb::evm::erc721::events::v1::Transfer;
-use common::is_zero_address;
+use substreams::log;
 use substreams_abis::evm::token::erc721;
 use substreams_ethereum::pb::eth::v2 as eth;
-use substreams_ethereum::Event;
+use substreams_ethereum::Event as _;
 
-/// Helper that extracts ERC721 transfer events from a block
-fn extract_erc721_events<'a, T, F>(blk: &'a eth::Block, process_event: F) -> impl Iterator<Item = T> + 'a
-where
-    F: Fn(u64, &[u8], u64, &[u8], erc721::events::Transfer) -> Option<T> + 'a + Copy,
-{
-    let block_num = blk.number;
+pub fn get_transfers<'a>(blk: &'a eth::Block) -> impl Iterator<Item = Transfer> + 'a {
     blk.receipts().flat_map(move |receipt| {
         let hash = &receipt.transaction.hash;
         let contract = &receipt.transaction.to;
         receipt.receipt.logs.iter().filter_map(move |log| {
             if let Some(event) = erc721::events::Transfer::match_and_decode(log) {
-                process_event(block_num, hash, log.block_index as u64, contract, event)
+                log::info!("log.address: {:?}, receipt.transaction.to: {:?}, trx: {:?}", log.address, contract, hash);
+                Some(Transfer {
+                    block_num: blk.number,
+                    tx_hash: hash.to_vec().into(),
+                    log_index: log.block_index as u64,
+                    contract: contract.to_vec().into(),
+                    from: event.from.to_vec().into(),
+                    to: event.to.to_vec().into(),
+                    token_id: event.token_id.to_string(),
+                    ..Default::default()
+                })
             } else {
                 None
             }
         })
-    })
-}
-
-pub fn get_all<'a>(blk: &'a eth::Block) -> impl Iterator<Item = Transfer> + 'a {
-    extract_erc721_events(blk, |block_num, hash, log_index, contract, event| {
-        Some(Transfer {
-            block_num,
-            tx_hash: hash.to_vec().into(),
-            log_index,
-            contract: contract.to_vec().into(),
-            from: event.from.to_vec().into(),
-            to: event.to.to_vec().into(),
-            token_id: event.token_id.to_string(),
-            ..Default::default()
-        })
-    })
-}
-
-#[allow(dead_code)]
-pub fn get_transfers<'a>(blk: &'a eth::Block) -> impl Iterator<Item = Transfer> + 'a {
-    extract_erc721_events(blk, |block_num, hash, log_index, contract, event| {
-        let from = &event.from;
-        let to = &event.to;
-
-        if !is_zero_address(from) && !is_zero_address(to) {
-            Some(Transfer {
-                block_num,
-                tx_hash: hash.to_vec().into(),
-                log_index,
-                contract: contract.to_vec().into(),
-                from: from.to_vec().into(),
-                to: to.to_vec().into(),
-                token_id: event.token_id.to_string(),
-                ..Default::default()
-            })
-        } else {
-            None
-        }
-    })
-}
-
-pub fn get_mints<'a>(blk: &'a eth::Block) -> impl Iterator<Item = Transfer> + 'a {
-    extract_erc721_events(blk, |block_num, hash, log_index, contract, event| {
-        let from = &event.from;
-        let to = &event.to;
-
-        if is_zero_address(from.as_ref() as &[u8]) {
-            Some(Transfer {
-                block_num,
-                tx_hash: hash.to_vec().into(),
-                log_index,
-                contract: contract.to_vec().into(),
-                to: to.to_vec().into(),
-                token_id: event.token_id.to_string(),
-                ..Default::default()
-            })
-        } else {
-            None
-        }
-    })
-}
-
-#[allow(dead_code)]
-pub fn get_burns<'a>(blk: &'a eth::Block) -> impl Iterator<Item = Transfer> + 'a {
-    extract_erc721_events(blk, |block_num, hash, log_index, contract, event| {
-        let to = &event.to;
-        let from = &event.from;
-        if is_zero_address(to.as_ref() as &[u8]) {
-            Some(Transfer {
-                block_num,
-                tx_hash: hash.to_vec().into(),
-                log_index,
-                contract: contract.to_vec().into(),
-                from: from.to_vec().into(),
-                token_id: event.token_id.to_string(),
-                ..Default::default()
-            })
-        } else {
-            None
-        }
     })
 }
--- erc721/src/lib.rs
+++ erc721/src/lib.rs
@@ -5,15 +5,13 @@ mod transactions;
 use common::is_zero_address;
 use pb::evm::erc721::events::v1::{Events, Transfer};
 use pb::evm::erc721::mints::v1::{Mints, Token};
-use std::collections::{HashMap, HashSet};
-use substreams::scalar::BigInt;
-use substreams_abis::evm::token::erc721::functions;
-use substreams_ethereum::{pb::eth::v2 as eth, rpc::RpcBatch};
+use std::collections::HashMap;
+use substreams_ethereum::pb::eth::v2 as eth;
 
 /// Extracts events events from the logs
 #[substreams::handlers::map]
 fn map_events(blk: eth::Block, mints: Mints) -> Result<Events, substreams::errors::Error> {
-    let mut transfers: Vec<Transfer> = events::get_all(&blk).collect();
+    let mut transfers: Vec<Transfer> = events::get_transfers(&blk).collect();
 
     // Merge metadata from mints into transfers
     merge_metadata(&mut transfers, &mints.tokens);
@@ -24,54 +22,6 @@ fn map_events(blk: eth::Block, mints: Mints) -> Result<Events, substreams::error
     let transactions = transactions::get_transactions(&blk, &event_tx_hashes);
 
     Ok(Events { transfers, transactions })
-}
-
-/// Extracts mints with uri, symbol and name from the logs
-/// We do this in a separate module to avoid re-processing RPC calls if we change something in map_events
-#[substreams::handlers::map]
-fn map_mints(blk: eth::Block) -> Result<Mints, substreams::errors::Error> {
-    let mints: Vec<Transfer> = events::get_mints(&blk).collect();
-    if mints.is_empty() {
-        return Ok(Mints { tokens: vec![] });
-    }
-
-    let contracts = mints.iter().map(|m| &m.contract).collect::<HashSet<_>>().into_iter().collect::<Vec<_>>();
-    let mut symbols: HashMap<&[u8], (Option<String>, Option<String>)> = HashMap::new();
-    for contract_chunk in contracts.chunks(10) {
-        let batch = contract_chunk.iter().fold(RpcBatch::new(), |batch, address| {
-            batch.add(functions::Name {}, address.to_vec()).add(functions::Symbol {}, address.to_vec())
-        });
-        let responses = batch.execute().expect("failed to execute symbol rpc batch").responses;
-        for (i, address) in contract_chunk.iter().enumerate() {
-            let name = RpcBatch::decode::<String, functions::Name>(&responses[i * 2]);
-            let symbol = RpcBatch::decode::<String, functions::Symbol>(&responses[i * 2 + 1]);
-            symbols.insert(address.as_ref(), (symbol, name));
-        }
-    }
-
-    let mut tokens = vec![];
-    for chunk in mints.chunks(100) {
-        let batch = chunk.iter().fold(RpcBatch::new(), |batch, mint| {
-            let token_id = mint.token_id.parse::<BigInt>().expect("invalid token_id");
-            batch.add(functions::TokenUri { token_id }, mint.contract.to_vec())
-        });
-        let responses = batch.execute().expect("failed to execute uri rpc batch").responses;
-        for (i, mint) in chunk.into_iter().enumerate() {
-            let uri = RpcBatch::decode::<String, functions::TokenUri>(&responses[i]);
-            let (symbol, name) = symbols.get(&mint.contract.as_ref()).cloned().unwrap_or((None, None));
-            tokens.push(Token {
-                uri,
-                symbol,
-                name,
-                contract: mint.contract.clone(),
-                token_id: mint.token_id.clone(),
-            });
-        }
-    }
-
-    substreams::log::info!("{} contracts, {} mints", contracts.len(), tokens.len());
-
-    Ok(Mints { tokens })
 }
 
 fn merge_metadata(transfers: &mut [Transfer], tokens: &[Token]) {
