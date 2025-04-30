cc89b29c801617f80a3873c7d55017c570998436
Denis Carriere <carriere.denis@gmail.com>
2025-04-30 12:21:29 -0400

    update schema

--- clickhouse-ens/schema.sql
+++ clickhouse-ens/schema.sql
@@ -186,6 +186,53 @@ PRIMARY KEY (timestamp, block_num, `index`)
 ORDER BY (timestamp, block_num, `index`);
 
 
+CREATE TABLE IF NOT EXISTS addresses (
+    global_sequence         UInt64, -- latest global sequence (block_num << 32 + index)
+    address                 FixedString(42),
+    node                    FixedString(66),
+
+    INDEX idx_node       (node)       TYPE bloom_filter GRANULARITY 4
+)
+ENGINE = ReplacingMergeTree(global_sequence)
+ORDER BY (address, node);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS address_changed_mv
+TO addresses AS
+SELECT
+    global_sequence,
+    node,
+    address
+FROM address_changed
+WHERE contract IN ('0x231b0ee14048e9dccd1d247744d114a4eb5e8e63', '0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41') -- ENS: Public Resolver
+
+-- INSERT INTO address_changed SELECT * FROM address_changed;
+-- SELECT node, address FROM addresses;
+
+CREATE TABLE IF NOT EXISTS names (
+    node                    FixedString(66),
+    name                    String,
+    registered              SimpleAggregateFunction(min, DateTime(0, 'UTC')),
+    expires                 SimpleAggregateFunction(max, DateTime(0, 'UTC')),
+
+    INDEX idx_name       (name)       TYPE bloom_filter GRANULARITY 4
+)
+ENGINE = AggregatingMergeTree
+ORDER BY (node, name);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS name_registered_mv
+TO names AS
+SELECT
+    node,
+    name,
+    min(timestamp) as registered,
+    max(expires) as expires
+FROM name_registered
+WHERE contract IN ('0x253553366da8546fc250f225fe3d25d0c782303b') -- ENS: ETH Registrar Controller
+GROUP BY node, name;
+
+-- INSERT INTO name_registered SELECT * FROM name_registered;
+-- SELECT node, name, expires FROM names WHERE expires > now();
+
 CREATE TABLE IF NOT EXISTS records (
     global_sequence      UInt64, -- latest global sequence (block_num << 32 + index)
     node                 FixedString(66),
@@ -209,54 +256,22 @@ SELECT
 FROM text_changed
 WHERE contract IN ('0x231b0ee14048e9dccd1d247744d114a4eb5e8e63', '0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41'); -- ENS: Public Resolver
 
-CREATE TABLE IF NOT EXISTS names (
-    node                    FixedString(66),
-    name                    String,
-    registered              SimpleAggregateFunction(min, DateTime(0, 'UTC')),
-    expires                 SimpleAggregateFunction(max, DateTime(0, 'UTC')),
-
-    INDEX idx_name       (name)       TYPE bloom_filter GRANULARITY 4
+CREATE TABLE IF NOT EXISTS agg_records (
+    node                FixedString(66),
+    kv_pairs_state      AggregateFunction(groupArray, Tuple(String, String))
 )
 ENGINE = AggregatingMergeTree
-ORDER BY (node, name);
+ORDER BY (node);
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS name_registered_mv
-TO names AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS agg_records_mv
+TO agg_records AS
 SELECT
     node,
-    name,
-    min(timestamp) as registered,
-    max(expires) as expires
-FROM name_registered
-WHERE contract IN ('0x253553366da8546fc250f225fe3d25d0c782303b') -- ENS: ETH Registrar Controller
-GROUP BY name, node;
+    groupArrayState( (key, value) )  AS kv_pairs_state
+FROM records
+GROUP BY node;
 
-CREATE TABLE IF NOT EXISTS addresses (
-    global_sequence         UInt64, -- latest global sequence (block_num << 32 + index)
-    address                 FixedString(42),
-    node                    FixedString(66),
-
-    INDEX idx_node       (node)       TYPE bloom_filter GRANULARITY 4
-)
-ENGINE = ReplacingMergeTree(global_sequence)
-ORDER BY (address, node);
-
-CREATE MATERIALIZED VIEW IF NOT EXISTS address_changed_mv
-TO addresses AS
-SELECT
-    global_sequence,
-    node,
-    address
-FROM address_changed
-WHERE contract IN ('0x231b0ee14048e9dccd1d247744d114a4eb5e8e63', '0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41') -- ENS: Public Resolver
-
--- -- INSERTS --
--- INSERT INTO name_registered SELECT * FROM name_registered;
--- INSERT INTO address_changed SELECT * FROM address_changed;
-
--- SELECT address, name, expires FROM addresses
--- JOIN names FINAL USING (node)
--- WHERE expires > now()
--- ORDER BY expires ASC
--- LIMIT 10;
+-- INSERT INTO text_changed SELECT * FROM text_changed;
+-- SELECT node, groupArrayMerge(kv_pairs_state) FROM agg_records GROUP BY node;
+-- SELECT node, groupArray( (key, value) ) FROM records GROUP BY node;
 
