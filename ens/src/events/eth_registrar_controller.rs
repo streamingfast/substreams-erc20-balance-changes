f8b7859a5e13b1dbc101830ace8f20f0a3390db4
Denis Carriere <carriere.denis@gmail.com>
2025-04-30 13:37:37 -0400

    fix u64

--- ens/src/events/eth_registrar_controller.rs
+++ ens/src/events/eth_registrar_controller.rs
@@ -17,6 +17,11 @@ pub fn insert_eth_registrar_controller<'a>(events: &mut ens::Events, transaction
         } else {
             return;
         };
+        let base_cost = if bigint_to_uint64(&event.base_cost).is_some() {
+            event.base_cost.to_u64()
+        } else {
+            return;
+        };
         events.name_registered.push(ens::NameRegistered {
             contract: log.address.to_vec(),
             transaction_hash: transaction.hash.to_vec(),
@@ -25,7 +30,7 @@ pub fn insert_eth_registrar_controller<'a>(events: &mut ens::Events, transaction
             name: event.name,
             label: event.label.to_vec(),
             owner: event.owner.to_vec(),
-            base_cost: event.base_cost.to_u64(),
+            base_cost,
             expires,
             node: node.to_vec(),
         });
@@ -39,6 +44,11 @@ pub fn insert_eth_registrar_controller<'a>(events: &mut ens::Events, transaction
         } else {
             return;
         };
+        let cost = if bigint_to_uint64(&event.cost).is_some() {
+            event.cost.to_u64()
+        } else {
+            return;
+        };
 
         events.name_renewed.push(ens::NameRenewed {
             contract: log.address.to_vec(),
@@ -47,7 +57,7 @@ pub fn insert_eth_registrar_controller<'a>(events: &mut ens::Events, transaction
             ordinal: log.ordinal,
             name: event.name,
             label: event.label.to_vec(),
-            cost: event.cost.to_u64(),
+            cost,
             expires,
             node: node.to_vec(),
         });
