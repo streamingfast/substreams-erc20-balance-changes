a8de6d2808cd0a0848f2290f5fcbd0bf4e4d4766
Denis Carriere <carriere.denis@gmail.com>
2025-06-26 14:48:46 -0400

    Dissolve `erc20-metadata-functions` into `erc20-metadata`

--- erc20-metadata-functions/README.md
+++
@@ -1,7 +0,0 @@
-# `ERC20` Token Metadata via `functions`
-
-## Includes
-
-- name
-- symbol
-- decimals
--- erc20-metadata/src/lib.rs
+++ erc20-metadata/src/lib.rs
@@ -1,16 +1,20 @@
+mod abi;
 mod calls;
+mod functions;
 
 use std::collections::HashSet;
 
 use common::Address;
-use proto::pb::evm::erc20::metadata::v1::{Events, MetadataInitialize};
+use proto::pb::evm::erc20::metadata::v1::{Events, MetadataChanges, MetadataInitialize};
 use proto::pb::evm::erc20::stores::v1::Events as ERC20FirstTransfer;
+use substreams_ethereum::pb::eth::v2::Block;
 
 use crate::calls::{batch_decimals, batch_name, batch_symbol};
+use crate::functions::get_metadata;
 
 #[substreams::handlers::map]
-fn map_events(chunk_size: String, erc20: ERC20FirstTransfer, erc20_metadata_functions: Events) -> Result<Events, substreams::errors::Error> {
-    let mut events = erc20_metadata_functions;
+fn map_events(chunk_size: String, block: Block, erc20: ERC20FirstTransfer) -> Result<Events, substreams::errors::Error> {
+    let mut events = Events::default();
     let chunk_size = chunk_size.parse::<usize>().expect("Failed to parse chunk_size");
 
     let contracts: Vec<&Address> = erc20
@@ -36,6 +40,30 @@ fn map_events(chunk_size: String, erc20: ERC20FirstTransfer, erc20_metadata_func
                 symbol: symbols.remove(contract),
                 name: names.remove(contract),
             });
+        }
+    }
+
+    // Extended blocks ONLY since we need to extract data from calls
+    for trx in block.transactions() {
+        for call_view in trx.calls() {
+            let call = call_view.call;
+
+            if let Some(result) = get_metadata(call) {
+                events.metadata_changes.push(MetadataChanges {
+                    // -- transaction --
+                    tx_hash: trx.hash.to_vec(),
+
+                    // -- call --
+                    caller: call.caller.to_vec(),
+                    address: call.address.to_vec(),
+                    begin_ordinal: call.begin_ordinal,
+                    end_ordinal: call.end_ordinal,
+
+                    // -- function --
+                    name: result.name,
+                    symbol: result.symbol,
+                });
+            }
         }
     }
 
