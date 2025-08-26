1d5975c51eaba663db3a9b784548fe35eafb1a0a
Denis Carriere <carriere.denis@gmail.com>
2025-08-26 15:53:21 +0200

    add chunk size

--- native-balances/src/calls.rs
+++ native-balances/src/calls.rs
@@ -8,25 +8,27 @@ use substreams_ethereum::{
 };
 
 // Returns the token URI.
-pub fn batch_eth_balance_of<'a>(block: u64, owners: &'a [&Address], _chunk_size: usize) -> HashMap<&'a Address, BigInt> {
+pub fn batch_eth_balance_of<'a>(block: u64, owners: &'a [&Address], chunk_size: usize) -> HashMap<&'a Address, BigInt> {
     let mut results: HashMap<&Address, BigInt> = HashMap::with_capacity(owners.len());
 
-    let mut requests = RpcGetBalanceRequests {
-        requests: Vec::with_capacity(owners.len()),
-    };
-    for owner in owners {
-        requests.requests.push(RpcGetBalanceRequest {
-            address: owner.to_vec(),
-            block: block.to_string(),
-        });
-    }
-    let balances = eth_get_balance(&requests);
-    for (i, owner) in owners.iter().enumerate() {
-        let response = &balances.responses[i];
-        if response.failed {
-            continue;
+    for chunk in owners.chunks(chunk_size) {
+        let mut requests = RpcGetBalanceRequests {
+            requests: Vec::with_capacity(chunk.len()),
+        };
+        for owner in chunk {
+            requests.requests.push(RpcGetBalanceRequest {
+                address: owner.to_vec(),
+                block: block.to_string(),
+            });
         }
-        results.insert(owner, BigInt::from_unsigned_bytes_be(&response.balance));
+        let balances = eth_get_balance(&requests);
+        for (i, owner) in chunk.iter().enumerate() {
+            let response = &balances.responses[i];
+            if response.failed {
+                continue;
+            }
+            results.insert(owner, BigInt::from_unsigned_bytes_be(&response.balance));
+        }
     }
     results
 }
