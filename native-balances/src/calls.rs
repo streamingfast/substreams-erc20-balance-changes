3f5d510b63bd3cb429298ff4a3c0ede69d9f5f9a
Denis Carriere <carriere.denis@gmail.com>
2025-08-26 16:24:24 +0200

    format block as hex

--- native-balances/src/calls.rs
+++ native-balances/src/calls.rs
@@ -1,34 +1,35 @@
 use std::collections::HashMap;
 
 use common::Address;
-use substreams::scalar::BigInt;
+use substreams::{log, scalar::BigInt};
 use substreams_ethereum::{
     pb::eth::rpc::{RpcGetBalanceRequest, RpcGetBalanceRequests},
     rpc::eth_get_balance,
 };
 
 // Returns the token URI.
-pub fn batch_eth_balance_of<'a>(block: u64, owners: &'a [&Address], chunk_size: usize) -> HashMap<&'a Address, BigInt> {
-    let mut results: HashMap<&Address, BigInt> = HashMap::with_capacity(owners.len());
+pub fn batch_eth_balance_of<'a>(block: u64, accounts: &'a [&Address], chunk_size: usize) -> HashMap<&'a Address, BigInt> {
+    let mut results: HashMap<&Address, BigInt> = HashMap::with_capacity(accounts.len());
 
-    for chunk in owners.chunks(chunk_size) {
+    for chunk in accounts.chunks(chunk_size) {
         let mut requests = RpcGetBalanceRequests {
             requests: Vec::with_capacity(chunk.len()),
         };
-        for owner in chunk {
+        for account in chunk {
             requests.requests.push(RpcGetBalanceRequest {
-                address: owner.to_vec(),
-                block: block.to_string(),
+                address: account.to_vec(),
+                block: format!("{:#x}", block), // to hex
             });
         }
         let balances = eth_get_balance(&requests);
-        for (i, owner) in chunk.iter().enumerate() {
+        for (i, account) in chunk.iter().enumerate() {
             let response = &balances.responses[i];
             if response.failed {
                 continue;
             }
-            results.insert(owner, BigInt::from_unsigned_bytes_be(&response.balance));
+            results.insert(account, BigInt::from_unsigned_bytes_be(&response.balance));
         }
+        log::info!("Processed {}/{} balance requests", chunk.len(), accounts.len());
     }
     results
 }
